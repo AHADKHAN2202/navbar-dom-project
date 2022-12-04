bf7dd1a6cae7f507cf141b3a7eb520d958996ef0
AHADKHAN2202 <101731915+AHADKHAN2202@users.noreply.github.com>
2022-12-04 21:30:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const list5 = document.getElementById('list5');
 const icon1 = document.getElementById('star-icon');
 const icon2 = document.getElementById('google-drive');
 const icon3 = document.getElementById('circle-icon');
+const icon4 = document.getElementById('check-icon');
 
 
 list1.addEventListener('mouseover',function(){
@@ -128,14 +129,34 @@ list5.addEventListener('mouseout',function(){
    Object.assign(list5.style,styles);
 })
 
-icon1.addEventListener('click',function(){
+icon1.addEventListener('mouseover',function(){
    icon1.style.color = "yellow";
 })
 
-icon2.addEventListener('click',function(){
+icon1.addEventListener('mouseout',function(){
+   icon1.style.color = "initial";
+})
+
+icon2.addEventListener('mouseover',function(){
    icon2.style.color = "blue";
 })
 
-icon3.addEventListener('click',function(){
+icon2.addEventListener('mouseout',function(){
+   icon2.style.color = "initial";
+})
+
+icon3.addEventListener('mouseover',function(){
    icon3.style.color = "red";
 })
+
+icon3.addEventListener('mouseout',function(){
+   icon3.style.color = "initial";
+})
+
+icon4.addEventListener('mouseover',function(){
+   icon4.style.color = 'blue'
+})
+
+icon4.addEventListener('mouseout',function(){
+   icon4.style.color = "initial";
+});
